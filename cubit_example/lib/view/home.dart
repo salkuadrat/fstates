6c7e9039d908c5b7cd2a93eb0a2b5ef52c515ae4
Salman S <salkuadrat@gmail.com>
2021-08-07 23:14:24 +0700

    cleaning cubit

--- cubit_example/lib/view/home.dart
+++ cubit_example/lib/view/home.dart
@@ -39,7 +39,7 @@ class _HomeState extends State<Home> {
         title: Text('Cubit Example'),
       ),
       body: BlocBuilder<UsersCubit, UsersState>(
-        builder: (context, state) {
+        builder: (_, state) {
           if (state.isFailed) {
             return Center(child: Text('Fetching data failed.'));
           }
